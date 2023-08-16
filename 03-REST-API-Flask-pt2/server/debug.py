75c6af76cbe4aec071eb7446d04288a0ffed9ae9
stephenlambertfs <stephenolambert@gmail.com>
2023-08-16 10:35:59 -0600

    update the starter code

--- 03-REST-API-Flask-pt2/server/debug.py
+++ 03-REST-API-Flask-pt2/server/debug.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from app import app
-from models import db, Production, CastMember
+from models import db, Production, CrewMember
 # alternative to flask shell
 if __name__ == '__main__':
     import ipdb; ipdb.set_trace()
