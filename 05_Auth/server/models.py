f4efeec007ce04226430fb5d0576dba1a9d80bb2
stephenlambertfs <stephenolambert@gmail.com>
2023-08-25 10:52:57 -0600

    updated model

--- 05_Auth/server/models.py
+++ 05_Auth/server/models.py
@@ -17,6 +17,7 @@ class User(db.Model,SerializerMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique = True)
+    password = db.Column(db.String)
     # 6) Add the password hash attribute
     
     created_at = db.Column(db.DateTime, server_default=db.func.now())
