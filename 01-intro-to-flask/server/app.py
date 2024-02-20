f51ab4333d2d5350d70aaebed2a1044592dded31
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-20 12:49:45 -0700

    lecture complete

--- 01-intro-to-flask/server/app.py
+++ 01-intro-to-flask/server/app.py
@@ -4,11 +4,26 @@
     # Request-Response Cycle
     # Web Servers and WSGI/Werkzeug
 # from werkzeug.wrappers import Request, Response
-
+from werkzeug.wrappers import Request, Response
 # @Request.application
 # def application(request):
 #     # print(f'This web server is running at {request.remote_attr}')
 #     return Response("A WSGI create this response!")
+
+
+# @Request.application
+# def application(request):
+#     print(f"This web server is running at {request.remote_addr}")
+#     return Response("A WSGI made this response")
+
+# if __name__ == "__main__":
+#     from werkzeug.serving import run_simple
+#     run_simple(
+#         hostname="localhost",
+#         port=5555,
+#         application=application
+#     )
+
 
 # if __name__ == "__main__":
 #     from werkzeug.serving import run_simple
@@ -26,6 +41,11 @@
 	# `Migrate` from `flask_migrate`
 	# db and `Production` from `models`
 
+    
+from flask import Flask, jsonify, make_response, request
+from flask_migrate import Migrate
+from models import db, Production
+
 # 3. ✅ Initialize the App
     # Add `app = Flask(__name__)`
     
@@ -35,6 +55,12 @@
     # Set the migrations with `migrate = Migrate(app, db)`
     
     # Finally, initialize the application with `db.init_app(app)`
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+migrate = Migrate(app, db)
+db.init_app(app)
 
  # 4. ✅ Migrate 
 	# `cd` into the `server` folder
@@ -53,10 +79,27 @@
 
 # 12. ✅ Routes
     # Create your routes
+@app.route("/")
+def index():
+    return '<h1>Hello World!</h1>'
 
 # 13. ✅ Run the server with `flask run` and verify your route in the browser at `http://localhost:5000/`
 
 # 14. ✅ Create a dynamic route
+@app.route("/productions/<string:title>")
+def production(title):
+    production = Production.query.filter(Production.title == title).first()
+    production_response = {
+        "title": production.title,
+        "genre": production.genre,
+        "director": production.director
+    }
+    response = make_response(
+        jsonify(production_response),
+        200
+    )
+    return response
+
 
 # 15.✅ Update the route to find a `production` by its `title` and send it to our browser
     
@@ -67,11 +110,17 @@
         # `jsonify` will convert our query into JSON
     
 # 16.✅ View the path and host with request context
+@app.route("/context")
+def context():
+    return f"<h1>Path {request.path} Host: {request.host} </h1>"
 
 # 17.✅ Use the before_request request hook, what this hook does is up to you. You could hit a breakpoint, print something to server console or anything else you can think of.
+app.before_request
+def run_before():
+    print("Running function before request")
 
 # Note: If you'd like to run the application as a script instead of using `flask run`, uncomment the line below 
 # and run `python app.py`
 
-# if __name__ == '__main__':
-#     app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
--- 01-intro-to-flask/server/seed.py
+++ 01-intro-to-flask/server/seed.py
@@ -4,23 +4,34 @@
 # 5. ✅ Imports
     # app from app
     # db and Production from models
+from app import app
+from models import db, Production
 
 # 6. ✅ Initialize the SQLAlchemy instance with `db.init_app(app)`
 
 # 7. ✅ Create application context `with app.app_context():`
     # Info on application context: https://flask.palletsprojects.com/en/1.1.x/appcontext/
-
-
+with app.app_context():
+    
 # 8.✅ Create a query to delete all existing records from Production 
+    Production.query.delete()
    
 # 9.✅ Create some seeds for production and commit them to the database. 
-
+    productions = []
+    p1 = Production(title="Hamlet", genre="Drama", budget=100000.0, image="https://cdn.kobo.com/book-images/5fc4252b-1c4f-40ef-9975-22982c94f12c/1200/1200/False/hamlet-prince-of-denmark-23.jpg", description="Hamlet is melancholy, bitter, and cynical, full of hatred for his uncle's scheming and disgust for his mother's sexuality. A reflective and thoughtful young man who has studied at the University of Wittenberg, Hamlet is often indecisive and hesitant, but at other times prone to rash and impulsive acts.", director="Bill Shakespeare", ongoing=False)
+    p2 = Production(title="Hamilton", genre="Musical", budget=400000.0, image="https://www.foxtheatre.org/assets/img/ATL_2324_FOXGraphics_Web_HAM630x580-b9cc36d541.png", description="A revolutionary story of passion, unstoppable ambition, and the dawn of a new nation. HAMILTON is the epic saga that follows the rise of Founding Father Alexander Hamilton as he fights for honour, love, and a legacy that would shape the course of a nation.", director="Lin-Manuel", ongoing=True)
+    productions.append(p1)
+    productions.append(p2)
+    db.session.add_all(productions)
+    db.session.commit()
 # 10.✅ Run in terminal:
     # `python seed.py`
+
 # 11.✅ run `flask shell` in the terminal 
     # from app import app
     # from models import Production
     # Check the seeds by querying Production
+    
 # 12.✅ Navigate back to app.py  
     
     
